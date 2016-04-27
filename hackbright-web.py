6d6112405169ff3b9358c74323c1db416ecd0ef9
Hackbright Student <info@hackbrightacademy.com>
2016-04-26 18:00:31 -0700

    added student to web and student database. Added POST method to student_add html.

--- hackbright-web.py
+++ hackbright-web.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash
+from flask import Flask, request, render_template
 
 import hackbright
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/student")
 def get_student():
     """Show information about a student."""
-
+    
     github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
     html = render_template("student_info.html", 
@@ -31,20 +31,21 @@ def get_new_student():
     return render_template("student_add.html")
 
 
-# @app.route("/student-add", methods=['POST'])
-# def get_new_student():
-#     """Show form to add a new student."""
+@app.route("/student-added", methods=['POST'])
+def added_new_student():
+    """Show form to add a new student."""
 
-#     first_name = request.form.get('first')
-#     last_name = request.form.get('last')
-#     github = request.form.get('github')
+    first = request.form.get('first')
+    last = request.form.get('last')
+    github = request.form.get('github')
    
-#     first, last, github = hackbright.make_new_student(first_name, last_name, github)
+    hackbright.make_new_student(first, last, github)
 
-#     return render_template("student_add.html", 
-#                             first=first,
-#                             last=last,
-#                             github=github)
+    return render_template("student_added.html", 
+                            first=first,
+                            last=last,
+                            github=github)
+
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True)
